d1a648c17b831b8e8068b5bb09eaea368eedba73
Dennis Rausch <hermitmaster@icloud.com>
2024-12-12 15:29:04 -0600

    Fix dashboard and Navigator

--- nvim/lua/plugins/snacks.nvim.lua
+++ nvim/lua/plugins/snacks.nvim.lua
@@ -3,17 +3,12 @@ return {
     "folke/snacks.nvim",
     opts = {
       dashboard = {
-        formats = {
-          key = function(item)
-            return { { "[", hl = "special" }, { item.key, hl = "key" }, { "]", hl = "special" } }
-          end,
-        },
         sections = {
           { section = "header" },
           { title = "Recent Files", padding = 1 },
-          { section = "recent_files", cwd = true, limit = 10, padding = 1 },
+          { section = "recent_files", cwd = true, limit = 10, padding = 3 },
           { title = "Bookmarks", padding = 1 },
-          { section = "keys", padding = 1 },
+          { section = "keys", padding = 3 },
           { section = "startup" },
         },
       },
