aab97cc853576c2406f3da942ebb7de0bbaa1ef4
Dennis Rausch <hermitmaster@icloud.com>
2025-02-11 14:22:00 -0600

    Updates

--- nvim/lua/plugins/custom_plugins.lua
+++ nvim/lua/plugins/custom_plugins.lua
@@ -1,5 +1,6 @@
 return {
   {
+    -- Navigation between Neovim and Wezterm panes.
     "numToStr/Navigator.nvim",
     opts = {},
   },
--- nvim/lua/plugins/overrides.lua
+++ nvim/lua/plugins/overrides.lua
@@ -1,37 +1,13 @@
 return {
   {
-    -- Override the default Copilot source with the native blink copilot source.
-    -- The default source is a translation layer for a nvim-cmp source.
-    "saghen/blink.cmp",
-    dependencies = {
-      "fang2hou/blink-copilot",
-      {
-        -- Disable the default copilot source
-        "giuxtaposition/blink-cmp-copilot",
-        enabled = false,
-      },
-    },
-    opts = {
-      sources = {
-        providers = {
-          copilot = {
-            module = "blink-copilot",
-            opts = {
-              max_completions = 3,
-              max_attempts = 4,
-            },
-          },
-        },
-      },
-    },
-  },
-  {
-    -- Add the golangci-lint to the enabled LSP servers.
+    -- Add golangci_lint_ls to the enabled LSP servers.
     "neovim/nvim-lspconfig",
-    requires = {
-      "williamboman/mason.nvim",
-      opts = {
-        ensure_installed = { "golangci-lint" },
+    dependencies = {
+      {
+        "williamboman/mason.nvim",
+        opts = {
+          ensure_installed = { "golangci-lint" },
+        },
       },
     },
     opts = {
@@ -39,39 +15,6 @@ return {
         golangci_lint_ls = {},
       },
     },
-  },
-  {
-    -- Add the gci formatter to the formatters list for Go files.
-    "stevearc/conform.nvim",
-    requires = {
-      "williamboman/mason.nvim",
-      opts = {
-        ensure_installed = { "gci" },
-      },
-    },
-    opts = function(_, opts)
-      local goprivate = os.getenv("GOPRIVATE")
-      if goprivate == "hq-stash.corp.proofpoint.com" then
-        opts.formatters.gci = {
-          args = {
-            "write",
-            "--skip-generated",
-            "--skip-vendor",
-            "--custom-order",
-            "-s",
-            "standard",
-            "-s",
-            "default",
-            "-s",
-            "prefix(hq-stash.corp.proofpoint.com)",
-            "-s",
-            "prefix(github.com/PFPT)",
-            "$FILENAME",
-          },
-        }
-      end
-      table.insert(opts.formatters_by_ft.go, "gci")
-    end,
   },
   {
     -- Unhide filtered items in the filesystem tree.
@@ -93,5 +36,63 @@ return {
         preset = "super-tab",
       },
     },
+  },
+  {
+    {
+      -- Override the default Copilot source with the native blink copilot source.
+      -- The default source is a translation layer for a nvim-cmp source.
+      "saghen/blink.cmp",
+      dependencies = {
+        "fang2hou/blink-copilot",
+        {
+          -- Disable the default copilot source
+          "giuxtaposition/blink-cmp-copilot",
+          enabled = false,
+        },
+      },
+      opts = {
+        sources = {
+          providers = {
+            copilot = {
+              module = "blink-copilot",
+              opts = {
+                max_completions = 3,
+                max_attempts = 4,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    -- Add the gci formatter to the formatters list for Go files.
+    "stevearc/conform.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+      opts = {
+        ensure_installed = { "gci" },
+      },
+    },
+    opts = function(_, opts)
+      table.insert(opts.formatters_by_ft.go, "gci")
+
+      local goprivate = os.getenv("GOPRIVATE") or ""
+
+      if goprivate then
+        opts.formatters.gci = {
+          append_args = {
+            "--custom-order",
+            "-s",
+            "standard",
+            "-s",
+            "default",
+          },
+        }
+        for entry in string.gmatch(goprivate, "([^,]+)") do
+          vim.list_extend(opts.formatters.gci.append_args, { "-s", "prefix(" .. entry .. ")" })
+        end
+      end
+    end,
   },
 }
