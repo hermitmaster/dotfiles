ba5cfb717603ae582a2246d453d493f28eb914a7
Dennis Rausch <hermitmaster@icloud.com>
2026-08-24 16:08:15 -0500

    chore: Formatting

--- nvim/lua/plugins/overrides.lua
+++ nvim/lua/plugins/overrides.lua
@@ -1,34 +1,34 @@
 return {
-  {
-    -- Navigation between Neovim and Wezterm panes.
-    "numToStr/Navigator.nvim",
-    opts = {},
-  },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-      window = {
-        mappings = {
-          -- Neo-tree maps <Tab> to "select" (multi-select) buffer-locally, which
-          -- shadows the global <Tab> window-switch keymap. "noop" makes neo-tree
-          -- skip creating the buffer-local map so the global one falls through.
-          ["<Tab>"] = "noop",
-        },
-      },
-      filesystem = {
-        filtered_items = {
-          hide_dotfiles = false,
-          visible = true,
-        },
-      },
-    },
-  },
-  {
-    "saghen/blink.cmp",
-    opts = {
-      keymap = {
-        preset = "super-tab",
-      },
-    },
-  },
+	{
+		-- Navigation between Neovim and Wezterm panes.
+		"numToStr/Navigator.nvim",
+		opts = {},
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		opts = {
+			window = {
+				mappings = {
+					-- Neo-tree maps <Tab> to "select" (multi-select) buffer-locally, which
+					-- shadows the global <Tab> window-switch keymap. "noop" makes neo-tree
+					-- skip creating the buffer-local map so the global one falls through.
+					["<Tab>"] = "noop",
+				},
+			},
+			filesystem = {
+				filtered_items = {
+					hide_dotfiles = false,
+					visible = true,
+				},
+			},
+		},
+	},
+	{
+		"saghen/blink.cmp",
+		opts = {
+			keymap = {
+				preset = "super-tab",
+			},
+		},
+	},
 }
